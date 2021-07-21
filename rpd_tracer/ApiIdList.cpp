fcb3d2c0a855350bfc50b362c731138d83465ef2
Michael Wootton <michael.wootton@amd.com>
2021-07-21 16:11:23 -0500

    Add ApiIdList

---
+++ rpd_tracer/ApiIdList.cpp
@@ -0,0 +1,39 @@
+#include "ApiIdList.h"
+
+ApiIdList::ApiIdList()
+: m_invert(true)
+{
+  loadApiNames();
+}
+
+void ApiIdList::add(std::string apiName)
+{
+  auto it = m_ids.find(apiName);
+  if (it != m_ids.end())
+    m_filter[it->second] = 1;
+}
+void ApiIdList::remove(std::string apiName)
+{
+  auto it = m_ids.find(apiName);
+  if (it != m_ids.end())
+    m_filter.erase(it->second);
+}
+
+bool ApiIdList::loadUserPrefs()
+{
+  // FIXME: check an ENV variable that points to an exclude file
+  return false;
+}
+bool ApiIdList::contains(uint32_t apiId)
+{
+  return (m_filter.count(apiId) > 0) ? !m_invert : m_invert;  // XOR
+}
+
+void ApiIdList::loadApiNames()
+{
+  // Build lut from apiName to apiId
+  for (uint32_t i = 0; i < HIP_API_ID_NUMBER; ++i) {
+    m_ids[std::string(hip_api_name(i))] = i;
+  }
+}
+
---
+++ rpd_tracer/ApiIdList.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <unordered_map>
+
+#include <roctracer_hip.h>
+
+// Usage
+//   contains() are items you are interested in, i.e. matches the filter
+//   "normal mode": things you add() are the only things matching the filter
+//   invertMode() == true: All things match filter except what you add()
+
+class ApiIdList
+{
+public:
+  ApiIdList();
+  bool invertMode() { return m_invert; }
+  void setInvertMode(bool invert) { m_invert = invert; }
+  void add(std::string apiName);
+  void remove(std::string apiName);
+  bool loadUserPrefs();
+
+  bool contains(uint32_t apiId);
+
+private:
+  std::map<std::string, uint32_t> m_ids;
+  std::unordered_map<uint32_t, uint32_t> m_filter;
+  bool m_invert;
+
+  void loadApiNames();
+};
+
