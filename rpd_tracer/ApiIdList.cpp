bc597574bf2258a0297acde0f4b25bee2452a6ee
Michael Wootton <michael.wootton@amd.com>
2021-10-01 02:11:38 -0500

    Fix filtering for rocm4.5.  Filtering now requires rocm4.2+

--- rpd_tracer/ApiIdList.cpp
+++ rpd_tracer/ApiIdList.cpp
@@ -3,20 +3,19 @@
 ApiIdList::ApiIdList()
 : m_invert(true)
 {
-  loadApiNames();
 }
 
 void ApiIdList::add(std::string apiName)
 {
-  auto it = m_ids.find(apiName);
-  if (it != m_ids.end())
-    m_filter[it->second] = 1;
+  uint32_t cid = 0;
+  if (roctracer_op_code(ACTIVITY_DOMAIN_HIP_API, apiName.c_str(), &cid, NULL) == ROCTRACER_STATUS_SUCCESS)
+    m_filter[cid] = 1;
 }
 void ApiIdList::remove(std::string apiName)
 {
-  auto it = m_ids.find(apiName);
-  if (it != m_ids.end())
-    m_filter.erase(it->second);
+  uint32_t cid = 0;
+  if (roctracer_op_code(ACTIVITY_DOMAIN_HIP_API, apiName.c_str(), &cid, NULL) == ROCTRACER_STATUS_SUCCESS)
+    m_filter.erase(cid);
 }
 
 bool ApiIdList::loadUserPrefs()
@@ -28,12 +27,3 @@ bool ApiIdList::contains(uint32_t apiId)
 {
   return (m_filter.find(apiId) != m_filter.end()) ? !m_invert : m_invert;  // XOR
 }
-
-void ApiIdList::loadApiNames()
-{
-  // Build lut from apiName to apiId
-  for (uint32_t i = 0; i < HIP_API_ID_NUMBER; ++i) {
-    m_ids[std::string(hip_api_name(i))] = i;
-  }
-}
-
