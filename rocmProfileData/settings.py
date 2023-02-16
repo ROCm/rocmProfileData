cc512e9f843080303076714e35c3a57a6ea0e316
Michael Wootton <michael.wootton@amd.com>
2023-02-16 10:34:45 -0600

    Remove rtg intergration for public.  Remove secret key.  Instructions to recreate

--- rocmProfileData/settings.py
+++ rocmProfileData/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-$2nv0*^jkr+xnf70-j++^6^y-226dcpz)@&)s*x2h6qjcselw'
+# Recreate:
+# from django.core.management.utils import get_random_secret_key; get_random_secret_key()
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
